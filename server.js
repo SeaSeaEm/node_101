08b8ade149d6577820b5655ba7b160f1e04273d7
Caio Cesar <caioc.mota@gmail.com>
2022-04-12 21:16:43 -0300

    added error handler, async middleware, models, data to seed the database

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+
+const connectDb = require('./config/db');
 
 // Load env vars
-dotenv.config({ path: './Config/config.env' })
+dotenv.config({ path: './config/config.env' })
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDb();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_END} mode on port ${PORT}`));
+// body parser
+app.use(express.json());
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development')
+    app.use(morgan('dev'));
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+// add error handler
+app.use(errorHandler);
+
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_END} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
